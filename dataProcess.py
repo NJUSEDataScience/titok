748267a9c00c456592fe7a4edb0ee8f0bdcbfd6a
Your Name <2396914751@qq.com>
2020-07-28 18:57:48 +0800

    feat: update path of data

--- dataProcess.py
+++ dataProcess.py
@@ -2,7 +2,10 @@ import json
 from urllib import request
 import csv
 
-musicPath='D:/myMusic' ##用于下载音乐并保存
+musicPath='D:/myMusic'
+musicPath1='D:/myMusic/level1' ##用于下载音乐并保存
+musicPath2='D:/myMusic/level2'
+musicPath3='D:/myMusic/level3'
 dataPath='data.json'##获取自动化抓包的数据
 
 def cleanFile():
@@ -44,28 +47,47 @@ if __name__ == "__main__":
     contain = []
     like_datas = []
     likes=[]
-    bgm=[]
+    comment=[]
+    share=[]
+    bgm1=[]
+    bgm2=[]
+    bgm3=[]
     for i in range(0,len(data["res"])):
      temp = data["res"][i]
      for item in temp["aweme_list"]:
         musicName = musicPath + '/' + str(item["desc"]) + '.mp3'
         #downloadMusic(musicName)
         if (len(str(item["music"]['play_url']['uri']))>1):
-            bgm.append(str(item["music"]['play_url']['uri']))
+            if(item["statistics"]['share_count']<3000):
+                bgm1.append(str(item["music"]['play_url']['uri']))
+            elif(item["statistics"]['share_count']>3000 and item["statistics"]['share_count']<50000):
+                bgm2.append(str(item["music"]['play_url']['uri']))
+            else:
+                bgm3.append(str(item["music"]['play_url']['uri']))
         my_dict = {'title': item["desc"], 'BGMurl': str(item["music"]['play_url']['uri']),
                    'likesCount': item["statistics"]['digg_count'], 'commentsCount': item["statistics"]['comment_count'],
                    'downloadCount': item["statistics"]['download_count'],
                    'forwardCount': item["statistics"]['forward_count'], 'shareCount': item["statistics"]['share_count']}
         like_dict = {'likesCount': item["statistics"]['digg_count']}
         likes.append(item["statistics"]['digg_count'])
+        comment.append(item["statistics"]['comment_count'])
+        share.append(item["statistics"]['share_count'])
         like_datas.append(like_dict)
         like_datas.sort(key=getSortKey)
-        likes.sort()
         contain.append(my_dict)
         contain.sort(key=getSortKey)
         writeTxt(item)
-    for i,item in enumerate(bgm):
-        musicName = musicPath + '/' + str(i) + '.mp3'
+    for i,item in enumerate(bgm1):
+        musicName = musicPath1 + '/' + str(i) + '.mp3'
         request.urlretrieve(item, filename=musicName)
-    print(likes)
+    for i,item in enumerate(bgm2):
+        musicName = musicPath2 + '/' + str(i) + '.mp3'
+        request.urlretrieve(item, filename=musicName)
+    for i,item in enumerate(bgm3):
+        musicName = musicPath3+ '/' + str(i) + '.mp3'
+        request.urlretrieve(item, filename=musicName)
+    share.sort()
+    print(share)
+    print(share[30])
+    print(share[80])
     writeCSVByLikes(like_datas)
